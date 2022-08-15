22f0ed2ead6d3960058b1b08b3c14d789c00a69d
hasanfaragg <hassanfarag165@gmail.com>
2022-08-15 13:20:14 +0200

    Creating Messages Controller

---
+++ GuestBookChallenge/Controllers/MessagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GuestBookTest.MessagesControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+       
+
+    }
+}
