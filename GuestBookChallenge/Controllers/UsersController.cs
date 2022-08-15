4527379116335bf4afcfbb3686e198eb76ee7722
hasanfaragg <hassanfarag165@gmail.com>
2022-08-15 13:03:41 +0200

    Creating UserController

---
+++ GuestBookChallenge/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using GuestBookTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GuestBookTest.Controllers
+{
+  
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+  
+public class UsersController : ControllerBase
+    {
+       
+    }
+}
